d0ec956c6ee61c8db7303fe77325146e4e3c2ccc
Kunal Mishra <kunal_mishra1@persistent.com>
2024-10-11 17:12:23 +0530

    Implemented UI Layout and Desgin

--- BankTransactions/Models/Transaction.cs
+++ BankTransactions/Models/Transaction.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BankTransactions.Models
@@ -9,17 +10,31 @@ namespace BankTransactions.Models
         public int TransactionId { get; set; }
 
         [Column(TypeName ="nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [MaxLength(12, ErrorMessage = "Maximum 12 characters only.")]
+        [Required]
         public string AccountNumber { get; set; }
 
         [Column(TypeName = "nvarchar(50)")]
+        [DisplayName("Beneficiary Name")]
+        [Required]
         public string BeneficiaryName { get; set; }
 
         [Column(TypeName = "nvarchar(12)")]
+        [DisplayName("IFSC Code")]
+        [MaxLength(11, ErrorMessage = "Maximum 12 characters only.")]
+        [Required]
         public string IFCSCode { get; set; }
 
         [Column(TypeName = "nvarchar(50)")]
+        [DisplayName("Bank Name")]
+        [Required]
         public string BankName { get; set; }
+
+        [Required]
         public int Amount { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MMM-dd-yy}")]
         public DateTime Date { get; set; }
 
     }
